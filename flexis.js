34e97991cc1de05b325c0c5b3cbf747438c8e04c
Carlos Liam <carlos@aarzee.me>
2017-09-07 10:03:23 -0400

    Add tetromino shape and rotation data and demo

--- flexis.js
+++ flexis.js
@@ -10,7 +10,186 @@ document.addEventListener('DOMContentLoaded', () => {
     'hotpink'
   ];
 
+  const TETROMINOES = [
+    'i',
+    'o',
+    't',
+    'j',
+    'l',
+    's',
+    'z'
+  ];
+
+  const TETROMINO_COLORS = {
+    i: 'cyan',
+    o: 'yellow',
+    t: 'hotpink',
+    j: 'blue',
+    l: 'orange',
+    s: 'limegreen',
+    z: 'red'
+  };
+
+  const TETROMINO_SHAPES = {
+    i: [
+      [
+        [false, true, false, false],
+        [false, true, false, false],
+        [false, true, false, false],
+        [false, true, false, false]
+      ],
+      [
+        [false, false, false, false],
+        [true, true, true, true],
+        [false, false, false, false],
+        [false, false, false, false]
+      ],
+      [
+        [false, false, true, false],
+        [false, false, true, false],
+        [false, false, true, false],
+        [false, false, true, false]
+      ],
+      [
+        [false, false, false, false],
+        [false, false, false, false],
+        [true, true, true, true],
+        [false, false, false, false]
+      ]
+    ],
+    o: [
+      [
+        [true, true],
+        [true, true]
+      ],
+      [
+        [true, true],
+        [true, true]
+      ],
+      [
+        [true, true],
+        [true, true]
+      ],
+      [
+        [true, true],
+        [true, true]
+      ]
+    ],
+    t: [
+      [
+        [false, false, false],
+        [true, true, true],
+        [false, true, false],
+      ],
+      [
+        [false, true, false],
+        [false, true, true],
+        [false, true, false]
+      ],
+      [
+        [false, true, false],
+        [true, true, true],
+        [false, false, false]
+      ],
+      [
+        [false, true, false],
+        [true, true, false],
+        [false, true, false]
+      ]
+    ],
+    j: [
+      [
+        [false, false, false],
+        [true, true, true],
+        [false, false, true]
+      ],
+      [
+        [false, true, true],
+        [false, true, false],
+        [false, true, false]
+      ],
+      [
+        [true, false, false],
+        [true, true, true],
+        [false, false, false]
+      ],
+      [
+        [false, true, false],
+        [false, true, false],
+        [true, true, false]
+      ]
+    ],
+    l: [
+      [
+        [false, false, false],
+        [true, true, true],
+        [true, false, false]
+      ],
+      [
+        [false, true, false],
+        [false, true, false],
+        [false, true, true]
+      ],
+      [
+        [false, false, true],
+        [true, true, true],
+        [false, false, false]
+      ],
+      [
+        [true, true, false],
+        [false, true, false],
+        [false, true, false]
+      ]
+    ],
+    s: [
+      [
+        [false, true, true],
+        [true, true, false],
+        [false, false, false]
+      ],
+      [
+        [true, false, false],
+        [true, true, false],
+        [false, true, false]
+      ],
+      [
+        [false, false, false],
+        [false, true, true],
+        [true, true, false]
+      ],
+      [
+        [false, true, false],
+        [false, true, true],
+        [false, false, true]
+      ]
+    ],
+    z: [
+      [
+        [true, true, false],
+        [false, true, true],
+        [false, false, false]
+      ],
+      [
+        [false, true, false],
+        [true, true, false],
+        [true, false, false]
+      ],
+      [
+        [false, false, false],
+        [true, true, false],
+        [false, true, true]
+      ],
+      [
+        [false, false, true],
+        [false, true, true],
+        [false, true, false]
+      ]
+    ]
+  };
+
   const randomColor = () => COLORS[Math.random() * COLORS.length | 0];
+
+  const randomTetromino = () => TETROMINOES[Math.random() * TETROMINOES.length | 0];
 
   const root = document.getElementById('root');
 
@@ -57,9 +236,60 @@ document.addEventListener('DOMContentLoaded', () => {
     updateColors();
   }
 
-  setInterval(moveForward, 100);
+  // setInterval(moveForward, 100);
+
+  const coordToIndex = (x, y) => y * 10 + x;
 
 
+  let currentTopLeft = coordToIndex(4, 0);
 
+  let currentRotation = 0;
+  let currentTetromino = 'i';
+
+  const tetrominoPositions = (tetromino, rotation, topLeft) => {
+    const shape = TETROMINO_SHAPES[tetromino][rotation];
+    const positions = [];
+    for(let i = 0; i < shape.length; i++) {
+      for(let j = 0; j < shape[i].length; j++) {
+        if (shape[i][j]) positions.push(topLeft + coordToIndex(j, i));
+      }
+    }
+    return positions;
+  }
+
+  const putTetromino = (tetromino, rotation, topLeft) => {
+    tetrominoPositions(tetromino, rotation, topLeft).forEach(index => colorsArray[index] = TETROMINO_COLORS[tetromino]);
+  };
+
+  const removeTetromino = (tetromino, rotation, topLeft) => {
+    tetrominoPositions(tetromino, rotation, topLeft).forEach(index => colorsArray[index] = 'empty');
+  };
+
+  putTetromino(currentTetromino, currentRotation, currentTopLeft);
+  updateColors();
+
+  const rotate = () => {
+    removeTetromino(currentTetromino, currentRotation, currentTopLeft);
+    currentRotation = (currentRotation + 1) & 3;
+    putTetromino(currentTetromino, currentRotation, currentTopLeft);
+    // debugger;
+    updateColors();
+  };
+
+  let ticks = 0;
+  let tetrominoIndex = 0;
+
+  const update = () => {
+    if (ticks % 10 == 9) {
+      removeTetromino(currentTetromino, currentRotation, currentTopLeft);
+      tetrominoIndex = (tetrominoIndex + 1) % TETROMINOES.length;
+      currentTetromino = TETROMINOES[tetrominoIndex];
+      currentRotation = 0;
+      putTetromino(currentTetromino, currentRotation, currentTopLeft);
+    } else rotate();
+    ticks++;
+  }
+
+  setInterval(update, 500);
 
 });
